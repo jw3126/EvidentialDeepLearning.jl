a36a9703e63a622b3679709f072cdad18bcbf122
Documenter.jl <documenter@juliadocs.github.io>
2021-10-01 09:53:16 +0000

    build based on 3cbfa59

--- dev/examples/examples/basic_regression.jl
+++ dev/examples/examples/basic_regression.jl
@@ -1,4 +1,4 @@
-# # Hello World
+# # Basic Regression
 # This tutorial is a port of [hello_world.py](https://github.com/aamini/evidential-deep-learning/blob/d1d8e395fb083308d14fa92c5ce766e97b2a066a/hello_world.py) to julia.
 # We create a synthetic dataset, train a simple evidential regression model and plot the results
 using Flux, Plots
@@ -18,26 +18,13 @@ plot(title="Dataset", ylims=(-150,150), xlims=(-7,7))
 scatter!(vec(x_train), vec(y_train), label="train", markersize=1)
 plot!(vec(x_test), vec(y_test), label="truth")
 
-# Our model is a single hidden layer dense network, where the output is converted to NormalInverseGamma
-# distributions
+# Our model is a basic single hidden layer dense network, where the output is lifted to the evidential world:
 model = Flux.Chain(
     Dense(1, 64, Flux.relu),
     Dense(64, 4, identity),
-    EDL.NIGs_from_4channels
+    EDL.NIGRegressor(),
 )
-
-# At the end of the day, the output of our model `d = model(x)` encodes three interesting informations:
-# * A prediction of y, given by `mean(posterior_predictive(x))`
-# * An uncertainty estimate, given by `std(posterior_predictive(x))`
-# * The evidence `evidence(d)`, which encodes how certain the model is about the prediction.
-# In order to optimize the model, we need a loss function, that covers these. The loss consists of two parts:
-# * The first part is the negative log likelyhood of the data under the predictive posterior.
-# * The regulizer part penalizes the net for outputting high evidence in in presence of wrong predictions
-function loss(x, y)
-    d = model(x)
-    λ = 1f-2
-    EDL.Losses.nll(EDL.posterior_predictive.(d), y) + λ*EDL.Losses.evidence_regularizer(d,y)
-end
+loss(x, y) = EDL.regression_loss(model(x), y, λ=1f-2)
 # Lets train the model
 data = Flux.Data.DataLoader((x_train, y_train), batchsize=100, shuffle=true)
 opt = ADAM(5e-4) # training is quite sensitive to learning rate
